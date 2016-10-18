929be027fff661ca3776d42234a546fbf8d47034
Marco Tomás <marco.tomas@altitude.com>
2016-10-18 16:00:27 +0100

    tests

--- SpecFlowPages/Utils/ConstantsUtils.cs
+++ SpecFlowPages/Utils/ConstantsUtils.cs
@@ -9,6 +9,7 @@ namespace SpecFlowPages
     public class ConstantsUtils
     {
         public static string Url = "http://www.specflow.org/";
+
         public static string GetStartedExcelButtonText = "Get Started Now with SpecFlow+ Excel!";
     }
 }
